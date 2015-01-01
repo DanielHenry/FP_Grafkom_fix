5f6c6564cfe23941c0d36a706ca64b8d71ab0719
DanielHenry <daniel.henry.39@gmail.com>
2015-01-01 20:37:01 +0700

    tes

--- main.cpp
+++ main.cpp
@@ -1,37 +1,205 @@
 #include<GL/freeglut.h>
 
-void inisiasi()
+void init ( GLvoid )     // Create Some Everyday Functions
 {
-     glClearColor(1.0, 1.0, 1.0, 0.0);
-     glMatrixMode(GL_PROJECTION);
-     glLoadIdentity();
-     gluOrtho2D(0.0, 640.0, 0.0, 480.0);     
+
+  glShadeModel(GL_SMOOTH);							// Enable Smooth Shading
+	glClearColor(0.0f, 0.0f, 0.0f, 0.5f);				// Black Background
+	glClearDepth(1.0f);									// Depth Buffer Setup
+	glEnable(GL_DEPTH_TEST);							// Enables Depth Testing
+	glDepthFunc(GL_LEQUAL);								// The Type Of Depth Testing To Do
+   glEnable ( GL_COLOR_MATERIAL );
+	glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 }
 
-void menggambar()
+/*void gambar_bola()
 {
-     glClear(GL_COLOR_BUFFER_BIT);
-     
-     glColor3f(0.0, 0.0, 0.0);
-     glBegin(GL_LINE_STRIP);
-       glColor3f(0.0, 0.0, 0.0);
-       glVertex2i(100,50);
-       glVertex2i(100,130);
-       glVertex2i(150,130);
-     glEnd();
-     
-     glFlush();  
+          
+}*/
+
+void gambar_meja_tenis()
+{
+    glColor3f(0, 0, 1);
+    glPushMatrix();
+    //glScaled(2, 0.1, 1);
+    //glutSolidCube(0.8);
+    glPopMatrix();
+
+    //net
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(0, 0.045, 0.41);
+    //glScaled(0.05, 0.3, 0.05);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(0, 0.045, -0.41);
+    //glScaled(0.05, 0.3, 0.05);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslated(0, 0.045, 0);
+    //glScaled(0.05, 0.3, 1.6);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    //garis meja
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslated(0, 0.04, 0);
+    //glScaled(1.9, 0.01, 0.01);
+    //glutSolidCube(0.8);
+    glPopMatrix();
+
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslated(0, 0.04, 0.38);
+    //glScaled(1.9, 0.01, 0.01);
+    //glutSolidCube(0.8);
+    glPopMatrix();
+
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslated(0, 0.04, -0.38);
+    //glScaled(1.9, 0.01, 0.01);
+    //glutSolidCube(0.8);
+    glPopMatrix();
+
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslated(0.755, 0.04, 0);
+    //glScaled(0.01, 0.01, 0.95);
+    //glutSolidCube(0.8);
+    glPopMatrix();
+
+    glColor3f(1, 1, 1);
+    glPushMatrix();
+    glTranslated(-0.755, 0.04, 0);
+    //glScaled(0.01, 0.01, 0.95);
+    //glutSolidCube(0.8);
+    glPopMatrix();
+
+    //kaki meja
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(0.5, -0.18, 0.2);
+    //glScaled(0.05, 0.7, 0.05);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(-0.5, -0.18, 0.2);
+    //glScaled(0.05, 0.7, 0.05);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(-0.5, -0.18, -0.2);
+    //glScaled(0.05, 0.7, 0.05);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(0.5, -0.18, -0.2);
+    //glScaled(0.05, 0.7, 0.05);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    //tambahan kaki meja
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(0.5, -0.18, 0);
+    //glScaled(0.05, 0.05, 0.8);
+    //glutSolidCube(0.5);
+    glPopMatrix();
+
+    glColor3f(0, 0, 0);
+    glPushMatrix();
+    glTranslated(-0.5, -0.18, 0);
+    //glScaled(0.05, 0.05, 0.8);
+    //glutSolidCube(0.5);
+    glPopMatrix(); 
 }
 
-int main ()
+/*void gambar_bat()
 {
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-    glutInitWindowSize(640,480);
-    glutInitWindowPosition(100,150);
-    glutCreateWindow("coba cg1");
-    glutDisplayFunc(menggambar);
-    inisiasi();
+     
+}*/
+
+void display ( void )   // Create The Display Function
+{
+   glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear Screen And Depth Buffer
+   glLoadIdentity();									// Reset The Current Modelview Matrix
+   glPushMatrix();
+   
+   gambar_meja_tenis();
+   glLoadIdentity();
     
-    glutMainLoop();
-    return 0;    
+   glPopMatrix();
+
+   glutSwapBuffers ( );
+  // Swap The Buffers To Not Be Left With A Clear Screen
 }
+
+void reshape ( int w, int h )   // Create The Reshape Function (the viewport)
+{
+  glViewport     ( 0, 0, w, h );
+  glMatrixMode   ( GL_PROJECTION );  // Select The Projection Matrix
+  glLoadIdentity ( );                // Reset The Projection Matrix
+  if ( h==0 )  // Calculate The Aspect Ratio Of The Window
+     gluPerspective ( 80, ( float ) w, 1.0, 5000.0 );
+  else
+     gluPerspective ( 80, ( float ) w / ( float ) h, 1.0, 5000.0 );
+  glMatrixMode   ( GL_MODELVIEW );  // Select The Model View Matrix
+  glLoadIdentity ( );    // Reset The Model View Matrix
+}
+
+void keyboard ( unsigned char key, int x, int y )  // Create Keyboard Function
+{
+  switch ( key ) {
+    case 27:        // When Escape Is Pressed...
+      exit ( 0 );   // Exit The Program
+      break;        // Ready For Next Case
+    default:        // Now Wrap It Up
+      break;
+  }
+}
+
+void arrow_keys ( int a_keys, int x, int y )  // Create Special Function (required for arrow keys)
+{
+  switch ( a_keys ) {
+    case GLUT_KEY_UP:     // When Up Arrow Is Pressed...
+      glutFullScreen ( ); // Go Into Full Screen Mode
+      break;
+    case GLUT_KEY_DOWN:               // When Down Arrow Is Pressed...
+      glutReshapeWindow ( 500, 500 ); // Go Into A 500 By 500 Window
+      break;
+    default:
+      break;
+  }
+}
+
+int main ( int argc, char** argv )   // Create Main Function For Bringing It All Together
+{
+  glutInit            ( &argc, argv ); // Erm Just Write It =)
+  init ();
+  glutInitDisplayMode ( GLUT_RGB | GLUT_DOUBLE); // Display Mode
+  glutInitWindowSize  ( 500, 500 ); // If glutFullScreen wasn't called this is the window size
+  glutCreateWindow    ( "FP Grafkom" ); // Window Title (argv[0] for current directory as title)
+  glutFullScreen      ( );          // Put Into Full Screen
+  glutDisplayFunc     ( display );  // Matching Earlier Functions To Their Counterparts
+  glutReshapeFunc     ( reshape );
+  glutKeyboardFunc    ( keyboard );
+  glutSpecialFunc     ( arrow_keys );
+  glutIdleFunc			 ( display );
+  glutMainLoop        ( );          // Initialize The Main Loop
+  return 0;
+}
+
